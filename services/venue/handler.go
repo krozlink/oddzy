d75e170ca47ce8ae2a7d6068d0a0871fa78ebac6
Daniel Taylor <dmtaylor17@gmail.com>
2018-05-17 22:49:29 +1000

    Removed cli as micro web tool is working now Added mongodb support for venues service

--- services/venue/handler.go
+++ services/venue/handler.go
@@ -3,26 +3,51 @@ package main
 import (
 	"context"
 	proto "github.com/krozlink/oddzy/services/venue/proto"
+	"gopkg.in/mgo.v2"
 )
 
-type service struct{}
+type service struct {
+	session *mgo.Session
+}
+
+func (s *service) GetRepo() Repository {
+	return &VenueRepository{s.session.Clone()}
+}
 
 func (s *service) Get(ctx context.Context, req *proto.GetRequest, resp *proto.GetResponse) error {
 	return nil
 }
 
 func (s *service) List(ctx context.Context, req *proto.ListRequest, resp *proto.ListResponse) error {
-	resp.Venues = []*proto.Venue{
-		&proto.Venue{
-			Category: "cat1234",
-			Country:  "AUS",
-			Id:       "234523432",
-			Name:     "Test Venue",
-		}}
-	return nil
+	repo := s.GetRepo()
+	defer repo.Close()
+
+	venues, err := repo.List()
+	resp.Venues = venues
+	return err
 }
 
 func (s *service) Add(ctx context.Context, req *proto.AddRequest, resp *proto.AddResponse) error {
+	repo := s.GetRepo()
+	defer repo.Close()
+
+	venue := &proto.Venue{
+		Id:       req.Id,
+		Category: req.Category,
+		Country:  req.Country,
+		Name:     req.Name,
+		State:    req.State,
+	}
+
+	err := repo.Add(venue)
+	if err != nil {
+		resp.Created = false
+		return err
+	}
+
+	resp.Created = true
+	resp.Venue = venue
+
 	return nil
 }
 
--- services/venue/main.go
+++ services/venue/main.go
@@ -5,10 +5,34 @@ import (
 	proto "github.com/krozlink/oddzy/services/venue/proto"
 	micro "github.com/micro/go-micro"
 	_ "github.com/micro/go-plugins/registry/consul"
+	"log"
+	"os"
+)
+
+const (
+	defaultHost = "localhost:27017"
 )
 
 func main() {
-	fmt.Println("started")
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = defaultHost
+	}
+
+	log.Printf("Connecting to %s", host)
+	session, err := CreateSession(host)
+	defer session.Close()
+
+	if err != nil {
+		log.Fatalf("Error connecting to datastore %s: %v", host, err)
+	}
+
+	log.Printf("Successfully connected")
+
+	if err = session.Ping(); err != nil {
+		log.Fatalf("Error on ping: %v", err)
+	}
+
 	srv := micro.NewService(
 		micro.Name("oddzy.services.venue"),
 		micro.Version("latest"),
@@ -16,7 +40,7 @@ func main() {
 
 	srv.Init()
 
-	proto.RegisterVenueServiceHandler(srv.Server(), &service{})
+	proto.RegisterVenueServiceHandler(srv.Server(), &service{session})
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
 	}
