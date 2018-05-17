ee63e228b1dd8c7731ecafe9d233c80cefc3fc15
Daniel Taylor <dmtaylor17@gmail.com>
2018-05-17 23:00:29 +1000

    Removed test project no longer needed Added test script to add venues to db

---
+++ test/scripts/venues.js
@@ -0,0 +1,653 @@
+db.venues.insert([
+			{
+				"_id": "05292847-04ca-4d86-bf1c-b83887d6fe67",
+				"name": "Gloucester Park",
+				"state": "WA",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "055605ac-c120-4785-8344-7c7a4c36196a",
+				"name": "Santa Anita Park",
+				"state": "CA",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "06e762b5-333e-4077-9299-ced0bc3d1428",
+				"name": "Penola",
+				"state": "SA",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "08282646-f059-44a6-9715-bff55fc455ad",
+				"name": "Newbury",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "0cb71294-1027-4619-a525-7a66dd7435b7",
+				"name": "Newcastle Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "1068331f-32f8-42f6-b3ce-d8573358464e",
+				"name": "The Gardens",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "12568650-22bf-4a11-844f-fa185494e1a3",
+				"name": "Cambridge",
+				"state": "NZ",
+				"country": "NZ",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "1548e612-6af1-4a87-acb6-c61fa233304f",
+				"name": "Wagga",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "158f164d-5edb-48f8-ad5f-3af4286ac651",
+				"name": "Woodbine",
+				"state": "ON",
+				"country": "CA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "187318aa-a6ab-46c3-b758-b10d0ca86c75",
+				"name": "Dax",
+				"state": "FR",
+				"country": "FR",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "1bf23035-5c0a-47e5-a3cd-189187c1e835",
+				"name": "Geelong",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "1d57633c-a152-49d2-96fc-d83127b3cadf",
+				"name": "Belmont Park Usa",
+				"state": "NY",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "20e87b2f-5027-4a9a-bc57-fbba343b8c5e",
+				"name": "Riccarton",
+				"state": "NZ",
+				"country": "NZ",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "212cb0d5-ecc2-456e-961e-46f1f3a9ef48",
+				"name": "Cairns",
+				"state": "QLD",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "214e179b-ac3d-4efa-aa27-d2d9c55ed76a",
+				"name": "Perth Uk",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "218b33c5-342d-47bc-bbef-05546ca8dacb",
+				"name": "Redcliffe",
+				"state": "QLD",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "26fc8784-a3ac-4387-ae97-db8cd809097e",
+				"name": "Ovrevoll",
+				"state": "NOR",
+				"country": "NOR",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "27687780-1159-46cf-9788-f97c6a77cc88",
+				"name": "Ipswich",
+				"state": "QLD",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "2c248b16-1ece-483e-8e11-86c16fbb1a27",
+				"name": "Penn National",
+				"state": "PA",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "2fd9ea87-0e6c-4b7d-997d-20d6da4f352a",
+				"name": "Angle Park",
+				"state": "SA",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "33612182-29fc-4ddd-91e6-6617d7289d2b",
+				"name": "Mountaineer Park",
+				"state": "WV",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "3c089333-1d52-4525-9bc2-a42eddb2274f",
+				"name": "Pakenham",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "3c08f985-daf2-4cf4-b15e-4a472d81acf4",
+				"name": "Mandurah",
+				"state": "WA",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "41036055-b448-4471-b64b-518543066fd1",
+				"name": "Albion Park",
+				"state": "QLD",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "43a5c53f-2688-4baa-bb93-d3464e741cf4",
+				"name": "Thistledown",
+				"state": "OH",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "4799d6c6-93a4-4116-b8af-69a5498c23ef",
+				"name": "Longchamp",
+				"state": "FR",
+				"country": "FR",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "481cbd51-1b9f-4974-9f59-cb0f83712a3a",
+				"name": "Quirindi",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "4f475216-1fd4-44d6-9162-e5fdc3d9a8f5",
+				"name": "Presque Isle Downs",
+				"state": "PA",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "4f7d90ce-9d88-4bdd-9b78-3f0199b1afcd",
+				"name": "Hoosier Park",
+				"state": "IN",
+				"country": "USA",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "4fa8243d-3a5b-47b7-ba3b-3407216f5ae9",
+				"name": "Salisbury",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "54fd43ae-62e7-4fff-a114-1ebd5378e3a7",
+				"name": "Kawasaki",
+				"state": "JPN",
+				"country": "JPN",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "55115ba6-7797-4060-ab29-64c2ef58aa8b",
+				"name": "Albion Park",
+				"state": "QLD",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "559f865c-f6e5-47e0-8881-fe6c92f4a508",
+				"name": "Romford Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "562f0f79-ecf3-4c04-805c-4d074ab7965e",
+				"name": "Northfield Park",
+				"state": "OH",
+				"country": "USA",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "5701c923-8aa0-4678-9da4-49acdcc1b90e",
+				"name": "Belterra Park",
+				"state": "OH",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "5ab4bd2a-a751-4256-b2a1-9d547caf25fd",
+				"name": "Charlton",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "5adab327-6773-445c-ab59-906df7cc43c3",
+				"name": "Goulburn",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "5ae5afa0-2797-4117-982b-0630d2b3b847",
+				"name": "Louisiana Downs",
+				"state": "LA",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "601b1815-c62e-46a6-b4b9-cabad0eb5739",
+				"name": "Hobart",
+				"state": "TAS",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "613f771d-94cd-4a5b-83da-9d8d789823c2",
+				"name": "Bankstown",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "6169de69-2840-434c-a174-66a3043cd64d",
+				"name": "Golden Gate",
+				"state": "CA",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "6932eba9-6acb-4f6e-ba81-c384e845d146",
+				"name": "Kembla Grange",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "69a63763-aace-4331-9e89-8184e5d60217",
+				"name": "Kranji",
+				"state": "SGP",
+				"country": "SGP",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "6a2a6438-5747-4771-a17d-bc40523bfaef",
+				"name": "Ballarat",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "6bef7365-573a-46b3-899c-829521307f26",
+				"name": "Cambridge",
+				"state": "NZ",
+				"country": "NZ",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "6d2b3084-6cd5-454b-a8e9-91a4525b880b",
+				"name": "Crayford Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "74c1fa80-012f-4da7-b9ec-0d84fe90926e",
+				"name": "Casino",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "764e58a3-a0d2-4b09-b6a4-db218606f7d5",
+				"name": "Sheffield Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "787554ec-dcf7-4da2-b4d8-ff35bf23181e",
+				"name": "Gulfstream Park",
+				"state": "FL",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "79f0ab71-a9ab-49d1-aeb6-7b6d8cf35951",
+				"name": "Prairie Meadows",
+				"state": "IA",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "7aff4aa8-e07f-442d-8316-f665a0274798",
+				"name": "Tauherenikau",
+				"state": "NZ",
+				"country": "NZ",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "80b63a6b-2581-4cd9-be02-3f1111b81b76",
+				"name": "Hamilton Park Uk",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "825818ed-d4f7-4c09-ac34-921e3f1b5622",
+				"name": "Saint Malo",
+				"state": "FR",
+				"country": "FR",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "8499b35c-8725-433c-976b-d48f3caf1563",
+				"name": "Bendigo",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "84b6fdca-aceb-4b03-8092-35cb01b4ac6b",
+				"name": "Newmarket Uk",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "85e0a0db-e0b5-49e7-b529-8071bf0af1b6",
+				"name": "Kilmore",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "865fd0e2-0786-4e26-ac2b-98f07dc9fba2",
+				"name": "Chartres",
+				"state": "FR",
+				"country": "FR",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "91a977e1-b438-4534-b138-3cb24536d081",
+				"name": "Addington",
+				"state": "NZ",
+				"country": "NZ",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "95c5c6e3-4c53-41ea-8706-997b7be82883",
+				"name": "Hobart",
+				"state": "TAS",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "9687f695-4a58-4b1f-9324-ab41dc93499c",
+				"name": "Pimlico",
+				"state": "MD",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "9eb22211-f026-45cc-8c5e-10273469b247",
+				"name": "Evangeline Downs",
+				"state": "LA",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "9fe189d8-3d1e-4932-88a8-4f65b2a67336",
+				"name": "Henlow Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "a1189bc2-f3ed-4b72-a8fd-cbc67ac1f13c",
+				"name": "Addington",
+				"state": "NZ",
+				"country": "NZ",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "a9614e8c-bfd6-4f08-bfbf-b32365415c2e",
+				"name": "Clonmel",
+				"state": "IRE",
+				"country": "IRE",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "ab57e842-c2b1-4508-b1c8-dfa908a2db54",
+				"name": "Bendigo",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "ac181704-7522-4936-926e-04f5edac4607",
+				"name": "Monmore Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "b665825c-c053-4518-9b14-fa5cc12c8428",
+				"name": "Geelong",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "b9b5e81e-599c-406f-8a86-a1a4c8fe8f55",
+				"name": "York Uk",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "ba41228e-3125-475b-a1d0-4e1d779e40b1",
+				"name": "Hatrick",
+				"state": "NZ",
+				"country": "NZ",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "bf83b5fa-4e14-4466-816a-e6c6a30ea5b1",
+				"name": "Sunderland Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "c188c424-9842-4461-8b3e-03572b17bedb",
+				"name": "Vaal",
+				"state": "SAF",
+				"country": "SAF",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "cbd9d549-fe73-4efd-8bf3-d0c65b03278e",
+				"name": "Cranbourne",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "d0acc1ca-3548-4719-9145-a892a1e6e1c0",
+				"name": "Warragul",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "d0c5ea63-11b6-4a06-956c-7006043c89db",
+				"name": "Yarmouth Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "d281ed91-653d-46c2-9d0e-23b3d436e033",
+				"name": "Port Macquarie",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "d634a153-2ec6-4813-a1fd-83bfe3d26031",
+				"name": "Sale",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "d9bf49ec-2497-4ef7-ace2-486f60bae4b2",
+				"name": "Richmond",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "da509302-f437-4131-81f7-0df3d0cff5d5",
+				"name": "Hove Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "db1590cf-e258-4288-b459-a639514dcc5e",
+				"name": "Finger Lakes",
+				"state": "NY",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "deece3ec-e060-4add-a7de-367863a26a0d",
+				"name": "Traralgon",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "e1edc2d2-bb8f-4ff4-ab56-0816135ca399",
+				"name": "Charles Town",
+				"state": "WV",
+				"country": "USA",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "e497b065-561a-443b-ab8c-b4b9c16c4f56",
+				"name": "Maitland",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "e6d6d9ab-5b43-4c66-9153-ee234c5cfbdd",
+				"name": "Ballarat",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "e6e6e035-95a8-47e4-a8d0-b61d59e02e51",
+				"name": "Wagga",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "eab96f27-bd76-4d4e-b5e7-f954e59001cd",
+				"name": "Central Park Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "ee0a708d-fb8a-424d-a37f-742b08540713",
+				"name": "Mount Gambier",
+				"state": "SA",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "eedda055-7bf9-4f6e-9c7a-3d990bb1c5fa",
+				"name": "Northam",
+				"state": "WA",
+				"country": "AUS",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "efbe250d-87bc-4965-8bca-3d956ea6844b",
+				"name": "Sandown Park",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "f00be2d6-0109-4fb4-bda8-8dee3a759d51",
+				"name": "Penrith",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "161d9be2-e909-4326-8c2c-35ed71fb460b"
+			},
+			{
+				"_id": "f0517e42-74e0-4f0e-a97e-79ad873498e6",
+				"name": "Fontwell Park",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "4a2788f8-e825-4d36-9894-efd4baf1cfae"
+			},
+			{
+				"_id": "f3b3c234-a729-45e9-9e9a-31aa2e6e17f0",
+				"name": "Perry Barr Bags",
+				"state": "UK",
+				"country": "UK",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "f7cc8008-5d6c-4b8d-8b49-10031b391b74",
+				"name": "Healesville",
+				"state": "VIC",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			},
+			{
+				"_id": "fa9c9012-b622-4b52-b034-f72ff8bc8bf6",
+				"name": "Dapto",
+				"state": "NSW",
+				"country": "AUS",
+				"category_id": "9daef0d7-bf3c-4f50-921d-8e818c60fe61"
+			}
+])
