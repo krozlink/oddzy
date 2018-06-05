fc640a44c5cba2851e59bcb1d9b117c7ae862dd8
Daniel Taylor <dmtaylor17@gmail.com>
2018-06-05 22:14:35 +1000

    Continued to implement setup phase

--- services/srv/race-scraper/process.go
+++ services/srv/race-scraper/process.go
@@ -7,6 +7,7 @@ import (
 	racing "github.com/krozlink/oddzy/services/srv/racing/proto"
 	micro "github.com/micro/go-micro"
 	_ "github.com/micro/go-plugins/registry/consul"
+	"github.com/satori/go.uuid"
 	"strconv"
 	"strings"
 	"time"
@@ -20,6 +21,18 @@ type scrapeProcess struct {
 	scraper   Scraper
 	startDate time.Time
 	endDate   time.Time
+
+	meetingsByID     map[string]*racing.Meeting
+	meetingsBySource map[string]*racing.Meeting
+	racesByID        map[string]*racing.Race
+	racesBySource    map[string]*racing.Race
+}
+
+type externalRaceData struct {
+	existingMeetings []*racing.Meeting
+	existingRaces    []*racing.Race
+	newMeetings      []*racing.Meeting
+	newRaces         []*racing.Race
 }
 
 var raceTypes = []string{"horse-racing", "harness", "greyhounds"}
@@ -65,31 +78,44 @@ func newScrapeProcess() scrapeProcess {
 func setup(p *scrapeProcess) {
 	p.status = "SETUP"
 	p.startDate, p.endDate = getDateRange()
-	intM, intR, err := readInternal(p)
+	meetings, races, err := readInternal(p)
 	if err != nil {
 		fmt.Printf("An error occurred reading internal racing data - %v", err)
 	}
-	extM, extR, err := readExternal(p)
+
+	for _, m := range meetings {
+		p.meetingsByID[m.MeetingId] = m
+		p.meetingsBySource[m.SourceId] = m
+	}
+
+	for _, r := range races {
+		p.racesByID[r.RaceId] = r
+		p.racesBySource[r.SourceId] = r
+	}
+
+	data, err := readExternal(p)
 	if err != nil {
 		fmt.Printf("An error occurred reading external racing data - %v", err)
 	}
 
-	var newMeetings map[string]*racing.Meeting
-	var newRaces map[string]*racing.Race
-
-	for _, m := range extM {
-		if meetingExists(m.SourceId, intM) {
-			m.MeetingId = newId()
-			newMeetings[m.MeetingId] = m
+	for _, m := range data.existingMeetings {
+		current := p.meetingsByID[m.MeetingId]
+		if meetingChanged(current, m) {
+			updateMeeting(m)
 		}
 	}
 
-	for _, r := range extR {
-		if raceExists(r.SourceId, intR) {
-			r.RaceId = newId()
-			newRaces[r.RaceId] = r
+	for _, r := range data.existingRaces {
+		current := p.racesByID[r.RaceId]
+		if raceChanged(current, r) {
+			updateRace(r)
 		}
 	}
+
+	// TODO: call AddMeetings for everything in data.newMeetings
+	// TODO: call AddRaces for everything in data.newRaces
+	// TODO: Add all existing races with a null last_updated as well as all new races to a missingEvents queue
+
 	// STATUS - SETUP
 	// read all internal meeting data for scraping period (yesterday to 2 days from now)
 	//		ListMeetingsByDate
@@ -97,8 +123,10 @@ func setup(p *scrapeProcess) {
 	// read all external meeting data for scraping period (yesterday to 2 days from now)
 	// for each external meeting
 	// 		if the meeting doesnt exist internally (look up source id), create meeting id and add to new meetings list
+	// 		if the meeting does exist internally, do comparison and update if necessary (how?)
 	// for each race
 	// 		if the race doesnt exist internally (look up source id) create a race id and add to new races list
+	//		if the race does exist internally, do comparison and update if necessary (how?)
 
 	// update new meeting list with their race ids
 	// call AddMeetings with new meetings list
@@ -108,9 +136,8 @@ func setup(p *scrapeProcess) {
 
 }
 
-func readExternal(p *scrapeProcess) ([]*racing.Meeting, []*racing.Race, error) {
-	meetings := make([]*racing.Meeting, 1)
-	races := make([]*racing.Race, 1)
+func readExternal(p *scrapeProcess) (*externalRaceData, error) {
+	data := &externalRaceData{}
 
 	// read race calendars from scraping period
 	for _, t := range raceTypes {
@@ -120,20 +147,22 @@ func readExternal(p *scrapeProcess) ([]*racing.Meeting, []*racing.Race, error) {
 			}
 			cal, err := p.scraper.ScrapeRaceCalendar(t, d.Format("2006-01-02"))
 			if err != nil {
-				return nil, nil, fmt.Errorf("Unable to read race calendar for %v on %v - %v", t, d, err)
+				return nil, fmt.Errorf("Unable to read race calendar for %v on %v - %v", t, d, err)
 			}
 
-			m, r, err := parseRaceCalendar(d, t, cal)
+			d, err := processRaceCalendar(p, d, t, cal)
 			if err != nil {
-				return nil, nil, fmt.Errorf("Unable to parse race calendar for %v on %v - %v", t, d, err)
+				return nil, fmt.Errorf("Unable to process race calendar for %v on %v - %v", t, d, err)
 			}
 
-			meetings = append(meetings, m...)
-			races = append(races, r...)
+			data.existingMeetings = append(data.existingMeetings, d.existingMeetings...)
+			data.existingRaces = append(data.existingRaces, d.existingRaces...)
+			data.newMeetings = append(data.newMeetings, d.newMeetings...)
+			data.newRaces = append(data.newRaces, d.newRaces...)
 		}
 	}
 
-	return meetings, races, nil
+	return data, nil
 }
 
 func readInternal(p *scrapeProcess) ([]*racing.Meeting, []*racing.Race, error) {
@@ -162,44 +191,72 @@ func readInternal(p *scrapeProcess) ([]*racing.Meeting, []*racing.Race, error) {
 	return mResp.Meetings, rResp.Races, nil
 }
 
-func parseRaceCalendar(date time.Time, eventType string, c *RaceCalendar) ([]*racing.Meeting, []*racing.Race, error) {
-	meetings := make([]*racing.Meeting, 1)
-	races := make([]*racing.Race, 1)
+func processRaceCalendar(p *scrapeProcess, date time.Time, eventType string, c *RaceCalendar) (*externalRaceData, error) {
+	newMeetings := make([]*racing.Meeting, 1)
+	newRaces := make([]*racing.Race, 1)
+
+	existingMeetings := make([]*racing.Meeting, 1)
+	existingRaces := make([]*racing.Race, 1)
 
 	for _, rg := range c.RegionGroups {
 		for _, m := range rg.Meetings {
 			firstEvent := m.Events[0]
 
-			sourceID, err := getMeetingSourceID(firstEvent.DateWithYear, firstEvent.EventURL)
+			mSource, err := getMeetingSourceID(firstEvent.DateWithYear, firstEvent.EventURL)
 			if err != nil {
-				return nil, nil, fmt.Errorf("unable to create source id for date %v and url %v", firstEvent.DateWithYear, firstEvent.EventURL)
+				return nil, fmt.Errorf("unable to create source id for date %v and url %v", firstEvent.DateWithYear, firstEvent.EventURL)
 			}
+
 			meeting := &racing.Meeting{
 				Name:           m.MeetingName,
 				ScheduledStart: firstEvent.StartTime,
 				RaceType:       eventType,
 				Country:        m.RegionDescription,
-				SourceId:       sourceID,
+				SourceId:       mSource,
+				RaceIds:        make([]string, 1),
 			}
 
-			meetings = append(meetings, meeting)
+			if val, ok := p.meetingsBySource[mSource]; ok {
+				meeting.MeetingId = val.MeetingId
+				existingMeetings = append(existingMeetings, meeting)
+			} else {
+				meeting.MeetingId = uuid.Must(uuid.NewV4()).String()
+				newMeetings = append(newMeetings, meeting)
+			}
 
 			for _, e := range m.Events {
+				rSource := string(e.EventID)
 				race := &racing.Race{
+					MeetingId:      meeting.MeetingId,
 					Name:           e.EventName,
 					Number:         e.EventNumber,
 					Results:        e.Results,
 					ScheduledStart: e.StartTime,
 					Status:         getRaceStatusFromCalendar(e.IsAbandoned, e.Resulted, e.Results),
-					SourceId:       string(e.EventID),
+					SourceId:       rSource,
 				}
 
-				races = append(races, race)
+				if val, ok := p.racesBySource[rSource]; ok {
+					race.RaceId = val.RaceId
+					existingRaces = append(existingRaces, race)
+				} else {
+					race.RaceId = uuid.Must(uuid.NewV4()).String()
+					newRaces = append(newRaces, race)
+				}
+
+				meeting.RaceIds = append(meeting.RaceIds, race.RaceId)
 			}
 		}
 	}
 
-	return meetings, races, nil
+	data := &externalRaceData{
+		existingMeetings,
+		existingRaces,
+		newMeetings,
+		newRaces,
+	}
+
+	return data, nil
 }
 
 func parseRaceCard(c *RaceCard) ([]*racing.Selection, error) {
