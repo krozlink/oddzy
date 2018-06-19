0b8387dca97a12715947a24c52ddfbdf20bc9bd0
Daniel Taylor <dmtaylor17@gmail.com>
2018-06-19 16:53:26 +1000

    mocking out stats client

--- services/srv/racing/stats.go
+++ services/srv/racing/stats.go
@@ -4,7 +4,17 @@ import (
 	"gopkg.in/alexcesaro/statsd.v2"
 )
 
-var stats *statsd.Client
+var stats statsClient
+
+type statsdClient struct {
+	c statsd.Client
+}
+
+type statsClient interface {
+	NewTiming() statsd.Timing
+	Increment(bucket string)
+	Send(bucket string)
+}
 
 const (
 	statsDefaultAddress = "statsd:9125"
@@ -14,4 +24,16 @@ func getStats(address string) (*statsd.Client, error) {
 	return statsd.New(
 		statsd.Address(address),
 	)
+}
+
+func (s *statsdClient) NewTiming() statsd.Timing {
+	return s.c.NewTiming()
+}
+
+func (s *statsdClient) Increment(bucket string) {
+	s.c.Increment(bucket)
+}
+
+func (s *statsdClient) Send(bucket string) {
+	s.c.Send(bucket)
 }
