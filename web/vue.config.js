a03c4318077e63b19493523766f9f9f8460f38fc
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-05 01:42:36 +1000

    Web - renamed package, added vue config

---
+++ web/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  outputDir: undefined,
+  baseUrl: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
+
+  devServer: {
+    port: 8081
+  }
+}
