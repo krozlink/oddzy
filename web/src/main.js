c1006c21bded877b099de0a84e7d26d8ea730988
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-06 17:06:37 +1000

    Web - Added buefy library

--- web/src/main.js
+++ web/src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
+import 'buefy/lib/buefy.css';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
