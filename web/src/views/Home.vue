72eb49b6e048b2a9e916752e09cdf6d5eaab8220
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-07 23:04:01 +1000

    Web - Remade template header, added placeholder racing page

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Home</h1>
+      <h2 class="subtitle">This is the home page</h2>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+
   },
 };
 </script>
---
+++ web/src/views/Racing.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Racing</h1>
+      <h2 class="subtitle">This is the racing page</h2>
+    </div>
+  </section>
+</template>
