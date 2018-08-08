d8e7acbfdaca3a6732a36002cf6a6bcbac5fc47f
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-08 19:00:52 +1000

    Web - Started basic layout for racing page * Changed primary colour

--- web/src/views/Racing.vue
+++ web/src/views/Racing.vue
@@ -1,8 +1,93 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">Racing</h1>
-      <h2 class="subtitle">This is the racing page</h2>
+      <nav class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <h1>Today's Racing</h1>
+          </div>
+        </div>
+        <div class="level-right">
+          <div class="level-item">
+            <div class="buttons has-addons">
+              <span class="button is-primary is-light is-selected">Today</span>
+              <span class="button is-light">Tomorrow</span>
+              <span class="button is-light">Saturday</span>
+              <span class="button is-light">Calendar</span>
+            </div>
+          </div>
+          <div class="level-item">
+            <div class="buttons has-addons">
+              <span class="button is-primary is-light is-selected">All</span>
+              <span class="button is-light">Horses</span>
+              <span class="button is-light">Harness</span>
+              <span class="button is-light">Greyhounds</span>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+
+      <div class="columns race-catagory ">
+        <div class="column race-location is-one-fifth">
+          Flemington
+        </div>
+        <div class="column race-list">
+          <div class="columns">
+            <div class="column race-item">
+              Race 1
+            </div>
+            <div class="column race-item">
+              Race 2
+            </div>
+            <div class="column race-item">
+              Race 3
+            </div>
+            <div class="column race-item">
+              Race 4
+            </div>
+            <div class="column race-item">
+              Race 5
+            </div>
+            <div class="column race-item">
+              Race 6
+            </div>
+            <div class="column race-item">
+              Race 7
+            </div>
+            <div class="column race-item">
+              Race 8
+            </div>
+            <div class="column race-item">
+              Race 9
+            </div>
+            <div class="column race-item">
+              Race 10
+            </div>
+            <div class="column race-item">
+              Race 11
+            </div>
+            <div class="column race-item">
+              Race 12
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
+
+
+<style lang="scss" scoped>
+
+div.container .race-location {
+  background-color: lightblue;
+}
+
+div.container .race-item {
+  background-color: lightcoral;
+  border-style: solid;
+  border-width: 1px;
+}
+
+</style>
