4989a72b7f27142871474c0bd7ba8bda2f26f5e0
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-09 22:57:59 +1000

    Web - added PageHeader component. Fixed nginx routing. Added eslint config

--- web/src/router.js
+++ web/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Racing from './views/Racing.vue';
 
 Vue.use(Router);
 
@@ -11,14 +12,12 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      
     },
     {
       path: '/racing',
       name: 'racing',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Racing.vue'),
+      component: Racing, 
     },
   ],
 });
--- web/src/views/Racing.vue
+++ web/src/views/Racing.vue
@@ -1,180 +1,178 @@
 <template>
-  <section class="section">
-    <div class="container">
+  <div id="racing" class="container">
+    <div class="section columns header">
+      <div class="column is-one-third">
+        <h4 class="title is-4">Today's Racing</h4>
+      </div>
+      <div class="column">
+        <nav class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <div class="buttons has-addons">
+                <span class="button is-primary is-light is-selected">Today</span>
+                <span class="button is-light">Tomorrow</span>
+                <span class="button is-light">Saturday</span>
+                <span class="button is-light">
+                  <span class="icon is-large">
+                    <i class="fas fa-lg fa-calendar-alt"></i>
+                  </span>
+                  <span>Select Date</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div class="level-right">
+            <div class="level-item racetype-icons">
+              <div class="buttons has-addons">
+              <span class="button is-primary is-light is-selected">All</span>
+              <span class="button is-light">
+                <span class="icon">
+                  <i class="icon-thoroughbred fa-3x"></i>
+                </span>
+              </span>
+              <span class="button is-light">
+                <span class="icon">
+                  <i class="icon-harness fa-3x"></i>
+                </span>
+              </span>
+              <span class="button is-light">
+                <span class="icon">
+                  <i class="icon-greyhound fa-3x"></i>
+                </span>
+              </span>
+            </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+    <div class="section race-section">
       <div class="columns">
-        <div class="column is-one-third">
-          <h4 class="title is-4">Today's Racing</h4>
-        </div>
         <div class="column">
-          <nav class="level">
-            <div class="level-left">
-              <div class="level-item">
-                <div class="buttons has-addons">
-                  <span class="button is-primary is-light is-selected">Today</span>
-                  <span class="button is-light">Tomorrow</span>
-                  <span class="button is-light">Saturday</span>
-                  <span class="button is-light">
-                    <span class="icon is-large">
-                      <i class="fas fa-lg fa-calendar-alt"></i>
-                    </span>
-                    <span>Select Date</span>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <div class="level-right">
-              <div class="level-item racetype-icons">
-                <div class="buttons has-addons">
-                <span class="button is-primary is-light is-selected">All</span>
-                <span class="button is-light">
-                  <span class="icon">
-                    <i class="icon-thoroughbred fa-3x"></i>
-                  </span>
-                </span>
-                <span class="button is-light">
-                  <span class="icon">
-                    <i class="icon-harness fa-3x"></i>
-                  </span>
-                </span>
-                <span class="button is-light">
-                  <span class="icon">
-                    <i class="icon-greyhound fa-3x"></i>
-                  </span>
-                </span>
-              </div>
-              </div>
-            </div>
-          </nav>
+          <h6 class="title is-6">Horse Racing - Australia & New Zealand</h6>
         </div>
       </div>
 
-      <div class="section race-section">
-        <div class="columns">
-          <div class="column">
-            <h6 class="title is-6">Horse Racing - Australia & New Zealand</h6>
-          </div>
+      <div class="columns race-catagory ">
+        <div class="column race-location is-one-fifth">
+          Flemington
         </div>
-
-        <div class="columns race-catagory ">
-          <div class="column race-location is-one-fifth">
-            Flemington
-          </div>
-          <div class="column race-list">
-            <div class="columns">
-              <div class="column race-item">
-                Race 1
-              </div>
-              <div class="column race-item">
-                Race 2
-              </div>
-              <div class="column race-item">
-                Race 3
-              </div>
-              <div class="column race-item">
-                Race 4
-              </div>
-              <div class="column race-item">
-                Race 5
-              </div>
-              <div class="column race-item">
-                Race 6
-              </div>
-              <div class="column race-item">
-                Race 7
-              </div>
-              <div class="column race-item">
-                Race 8
-              </div>
-              <div class="column race-item">
-                Race 9
-              </div>
-              <div class="column race-item">
-                Race 10
-              </div>
-              <div class="column race-item">
-                Race 11
-              </div>
-              <div class="column race-item">
-                Race 12
-              </div>
+        <div class="column race-list">
+          <div class="columns">
+            <div class="column race-item">
+              Race 1
+            </div>
+            <div class="column race-item">
+              Race 2
+            </div>
+            <div class="column race-item">
+              Race 3
+            </div>
+            <div class="column race-item">
+              Race 4
+            </div>
+            <div class="column race-item">
+              Race 5
+            </div>
+            <div class="column race-item">
+              Race 6
+            </div>
+            <div class="column race-item">
+              Race 7
+            </div>
+            <div class="column race-item">
+              Race 8
+            </div>
+            <div class="column race-item">
+              Race 9
+            </div>
+            <div class="column race-item">
+              Race 10
+            </div>
+            <div class="column race-item">
+              Race 11
+            </div>
+            <div class="column race-item">
+              Race 12
             </div>
           </div>
         </div>
-
       </div>
-
-      <div class="section race-section">
-        <div class="columns">
-          <div class="column">
-            <h6 class="title is-6">Horse Racing - International</h6>
-          </div>
-        </div>
-
-        <div class="columns race-catagory ">
-          <div class="column race-location is-one-fifth">
-            Flemington
-          </div>
-          <div class="column race-list">
-            <div class="columns">
-              <div class="column race-item">
-                Race 1
-              </div>
-              <div class="column race-item">
-                Race 2
-              </div>
-              <div class="column race-item">
-                Race 3
-              </div>
-              <div class="column race-item">
-                Race 4
-              </div>
-              <div class="column race-item">
-                Race 5
-              </div>
-              <div class="column race-item">
-                Race 6
-              </div>
-              <div class="column race-item">
-                Race 7
-              </div>
-              <div class="column race-item">
-                Race 8
-              </div>
-              <div class="column race-item">
-                Race 9
-              </div>
-              <div class="column race-item">
-                Race 10
-              </div>
-              <div class="column race-item">
-                Race 11
-              </div>
-              <div class="column race-item">
-                Race 12
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
 
     </div>
-  </section>
+
+    <div class="section race-section">
+      <div class="columns">
+        <div class="column">
+          <h6 class="title is-6">Horse Racing - International</h6>
+        </div>
+      </div>
+
+      <div class="columns race-catagory ">
+        <div class="column race-location is-one-fifth">
+          Flemington
+        </div>
+        <div class="column race-list">
+          <div class="columns">
+            <div class="column race-item">
+              Race 1
+            </div>
+            <div class="column race-item">
+              Race 2
+            </div>
+            <div class="column race-item">
+              Race 3
+            </div>
+            <div class="column race-item">
+              Race 4
+            </div>
+            <div class="column race-item">
+              Race 5
+            </div>
+            <div class="column race-item">
+              Race 6
+            </div>
+            <div class="column race-item">
+              Race 7
+            </div>
+            <div class="column race-item">
+              Race 8
+            </div>
+            <div class="column race-item">
+              Race 9
+            </div>
+            <div class="column race-item">
+              Race 10
+            </div>
+            <div class="column race-item">
+              Race 11
+            </div>
+            <div class="column race-item">
+              Race 12
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
 </template>
 
 
 <style lang="scss" scoped>
 
-div.container .race-location {
-  background-color: lightblue;
-}
+// div.container .race-location {
+//   background-color: lightblue;
+// }
 
-div.container .race-item {
-  background-color: lightcoral;
-  border-style: solid;
-  border-width: 1px;
-}
+// div.container .race-item {
+//   background-color: lightcoral;
+//   border-style: solid;
+//   border-width: 1px;
+// }
 
 .racetype-icons .button {
   width: 70px;
@@ -184,11 +182,12 @@ div.container .race-item {
   padding-top: 5px;
 }
 
-.race-section {
-  // border-style: solid;
-  // border-width: 1px;
-
+#racing .section  {
   padding-top: 16px;
+}
+
+#racing .header  {
+  padding-bottom: 0px;
 }
 
 </style>
