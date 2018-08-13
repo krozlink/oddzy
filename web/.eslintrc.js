7cc0e5f6d0768f1817fc06b7dd494ac1490a6294
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-13 22:33:52 +1000

    Web - fixed auto-linting

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,26 +1,27 @@
 module.exports = {
-    extends: [
-      // add more generic rulesets here, such as:
-      '@vue/airbnb',
-      'plugin:vue/essential'
-    ],
-    rules: {
-     'linebreak-style': ["error", "windows"],
-     'no-param-reassign': ['error', {
-        props: true,
-        ignorePropertyModificationsFor: [
-          'state', // for vuex state
-          'acc', // for reduce accumulators
-          'e', // for e.returnvalue
-          'ctx', // for Koa routing
-          'req', // for Express requests
-          'request', // for Express requests
-          'res', // for Express responses
-          'response', // for Express responses
-          '$scope', // for Angular 1 scopes
-        ]
-      }],
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    }
+  extends: [
+    // add more generic rulesets here, such as:
+    '@vue/airbnb',
+    'plugin:vue/essential'
+  ],
+  rules: {
+    'max-len': ["off"],
+    'linebreak-style': ["error", "windows"],
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e', // for e.returnvalue
+        'ctx', // for Koa routing
+        'req', // for Express requests
+        'request', // for Express requests
+        'res', // for Express responses
+        'response', // for Express responses
+        '$scope', // for Angular 1 scopes
+      ]
+    }],
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
   }
+}
