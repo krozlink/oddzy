df364d35a973035158de59152770dd2a5446a1d0
Daniel Taylor <dmtaylor17@gmail.com>
2018-08-19 01:39:58 +1000

    Web - racecard now displays results * Prices added but are disabled unless race is open

--- web/src/components/racing/RaceSelection.vue
+++ web/src/components/racing/RaceSelection.vue
@@ -3,26 +3,55 @@
             'scratched': selection.is_scratched
         }">
         <th class="sel-number">
-            {{selection.number}}
+            <div>
+                {{selection.number}}
+            </div>
+            <div class="result">
+                <span v-if="placed || win" :class="{
+                    'tag': true,
+                    'is-warning': win,
+                    'is-success': placed,
+                }">{{result}}<sup>{{resultOrdinal}}</sup></span>
+            </div>
         </th>
         <td class="sel-img">
             <img :src="selection.image_url" class="image" alt="">
         </td>
         <td class = "sel-info">
-            <div class="name">{{selection.name}} ({{selection.barrier}})</div>
-            <div class="jockey"><span class="j-icon">J</span>{{selection.jockey}} {{selection.jockey_weight}}</div>
+            <div class="name">{{selection.name}} <span v-if="selection.barrier">({{selection.barrier}})</span></div>
+            <div v-if="selection.jockey" class="jockey"><span class="j-icon">J</span>{{selection.jockey}} {{selection.jockey_weight}}</div>
         </td>
         <td class="price fixed win">
-            4.00
+            <div :class="{
+                button: true,
+                'is-disable': race.status !== 'OPEN'
+                }">
+                4.00
+            </div>
         </td>
         <td class="price fixed place">
-            1.40
+            <div :class="{
+                button: true,
+                'is-disable': race.status !== 'OPEN'
+                }">
+                1.15
+            </div>
         </td>
         <td class="price tote win">
-            3.20
+            <div :class="{
+                button: true,
+                'is-disable': race.status !== 'OPEN'
+                }">
+                6.80
+            </div>
         </td>
-        <td class="price stote place">
-            1.10
+        <td class="price tote place">
+            <div :class="{
+                button: true,
+                'is-disable': race.status !== 'OPEN'
+                }">
+                1.10
+            </div>
         </td>
     </tr>
 </template>
@@ -30,14 +59,88 @@
 <script>
 export default {
   name: 'RaceSelection',
-  props: ['selection'],
+  props: ['selection', 'race', 'meeting'],
+  computed: {
+    open() {
+      return this.race.status === 'OPEN';
+    },
+    win() {
+      const results = this.race.results.split(',');
+      return results.length > 0 && this.selection.number.toString() === results[0];
+    },
+    placed() {
+      const results = this.race.results.split(',');
+      return results.length > 0 && results.slice(1).includes(this.selection.number.toString());
+    },
+    result() {
+      const results = this.race.results.split(',');
+      return results.indexOf(this.selection.number.toString()) + 1;
+    },
+    resultOrdinal() {
+      return this.getOrdinal(this.result);
+    },
+  },
+  methods: {
+    getOrdinal(result) {
+      if (result === 1) {
+        return 'st';
+      } else if (result === 2) {
+        return 'nd';
+      } else if (result === 3) {
+        return 'rd';
+      }
+      return 'th';
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+
+.price.fixed.place {
+    border-right: 1px;
+    border-right-style: solid;
+    border-right-color: $grey-lighter
+}
+
+.price {
+    min-width: 70px;
+}
+
+.price div {
+    border-style: solid;
+    border-color: #AAA;
+    border-width: 2px;
+    width: 55px;
+    height: 30px;
+    margin-top: 10px;
+
+    align-self: center;
+    vertical-align: middle;
+
+    display:flex;
+    align-items: center; /* Vertical center alignment */
+    justify-content: center; /* Horizontal center alignment */
+
+    cursor: pointer;
+}
+
+.price div:hover{
+    background-color: $primary;
+    color: white;
+    border-color: $primary;
+}
+
+
 tr{
     height: 70px;
 }
+
+.sel-number div {
+    text-align: center;
+    width: 30px;
+}
+
 
 .sel-info {
     width: 100%;
@@ -51,15 +154,11 @@ tr{
     font-size: 0.7em;
 }
 
-.scratched td {
-    text-decoration: underline;
-}
-
 .j-icon {
     font-family: 'Quicksand';
     font-size: 0.8em;
     display: inline-block;
-    background-color: #04518D;
+    background-color: $primary;
     color: white;
     width: 12px;
     text-align: center;
