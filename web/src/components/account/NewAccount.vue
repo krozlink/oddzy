1156563414a9b8c74dcc4c2270cf7f080c063aa9
Daniel Taylor <dmtaylor17@gmail.com>
2018-10-02 16:58:24 +1000

    started NewAccount modal

---
+++ web/src/components/account/NewAccount.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="modal">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="field">
+                <label class="label">First Name</label>
+                <div class="control">
+                    <input type="text" placeholder="First Name">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Last Name</label>
+                <div class="control">
+                    <input type="text" placeholder="Last Name">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">User Name</label>
+                <div class="control">
+                    <input type="text" placeholder="User Name">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                    <input type="password" placeholder="Password">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input type="email" placeholder="Email">
+                </div>
+            </div>
+        </div>
+        <div class="modal-close is-large"></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
