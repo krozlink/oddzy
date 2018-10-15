663f75c3dc05421fe2cd2a9662889432543f8028
Daniel Taylor <dmtaylor17@gmail.com>
2018-10-15 21:31:44 +1000

    Updated betslip item styling

--- web/src/components/betslip/BetslipItem.vue
+++ web/src/components/betslip/BetslipItem.vue
@@ -1,7 +1,27 @@
 <template>
     <div class="card">
       <div class="card-header-title">
-        Redzel (win fixed)
+        <div class="bsi-header">
+          <div class="bsi-row">
+            <div class="entrant">
+              <div class="runner">
+                Redzel
+              </div>
+              <div class="meeting">
+                Flemington R6
+              </div>
+            </div>
+            <div class="odds">
+              <div class="price">
+                8.40
+              </div>
+              <div class="bet-type">
+                Win Fixed
+              </div>
+            </div>
+            <button class="delete is-small"></button>
+          </div>
+        </div>
       </div>
       <div class="card-content">
         something
@@ -26,5 +46,46 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.card {
+  margin-left: 1px;
+  margin-right: 1px;
+}
+.bsi-header {
+  display:table;
+  width: 100%;
+}
+
+.bsi-row {
+  display:table-row;
+}
+
+.bsi-row .delete {
+  float:right;
+}
+
+.entrant, .odds {
+  display:table-cell;
+}
+
+.meeting,.bet-type {
+  font-weight: 400;
+  font-size: 0.8em;
+  text-transform: uppercase;
+}
+
+.odds{
+  text-align: right;
+}
+
+.runner,.price {
+  display:inline-block;
+  position: relative;
+}
+
+.card-header-title {
+  display:inline-block;
+  width: 100%;
+}
 
 </style>
